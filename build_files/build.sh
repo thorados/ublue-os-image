e11eaa4b8837617429f51f1dc267f0fd1d7bd600
Thorados <web@dobberstein.it>
2025-12-15 15:35:46 +0100

    cosmic

--- build_files/build.sh
+++ build_files/build.sh
@@ -20,10 +20,11 @@ dnf5 install -y tmux chezmoi
 # dnf5 -y copr disable ublue-os/staging
 
 # remove kde plasma
-dnf5 -y remove plasma-workspace plasma-* kde-* @gnome-desktop
+dnf5 -y remove plasma-workspace plasma-* kde-*
+dnf5 -y group remove workstation-product-environment @kde-desktop-environment @gnome-desktop
 
 # install cosmic-desktop
-dnf5 -y @cosmic-desktop-environment
+dnf5 -y group install @cosmic-desktop-environment
 
 # install hyprland
 dnf5 -y copr enable solopasha/hyprland
@@ -53,6 +54,10 @@ dnf5 -y install     \
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
+
+
+
+
 
 
 
