309637196993f51c5e0b1dcf462d202ef916bf34
Thorados <web@dobberstein.it>
2025-12-15 22:22:44 +0100

    steam

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux chezmoi neovim btop zsh
+dnf5 install -y tmux chezmoi neovim btop zsh steam mangohud gamescope
 
 # Use a COPR Example:
 #
